afb5242b356307ea96ad80883110afa7542f999b
Polokego <pomakga022@student.wethinkcode.co.za>
2024-01-07 19:44:18 +0200

    Finished Kruskals algo of finding minimum spanning tree

--- kruskals_algorithm_mst.py
+++ kruskals_algorithm_mst.py
@@ -4,11 +4,12 @@ class Vertex:
         self.storage = storage;
         self.nextVertex = [];
 
-
 class Edge:
-    def __init__(self,edge, vertex2):
+    def __init__(self,edge,vertex, vertex2):
+        self.vertex = vertex;
         self.edge = edge;
         self.vertex2 = vertex2;
+    
 
 
 class Graph:
@@ -17,15 +18,70 @@ class Graph:
     def __init__(self):
         ''''''
         self.startNode = None;
+        self.allEdges= []
+
 
     def addNewEdge(self, vec1, vec2, edge):
         ''''''
         if (self.startNode == None):
             self.startNode = vec1;
-        vec1.nextVertex.append( Edge(edge, vec2));    
-    
-    def printMinSpanningTree(self):
-        ''''''
+        self.allEdges.append( Edge(edge, vec1, vec2));
+        vec1.nextVertex.append( self.allEdges[len(self.allEdges) - 1]);
+
+
+    def isCycle(self, listOfEdges, newEdge):
+        tempNode = newEdge.vertex;
+
+        ''''listOfVertices = [] + listOfEdges[0].vertex;
+        for vertex in listOfEdges:
+            listOfVertices.append(vertex.vertex2);
+        '''
+
+        #Transverse graph and view cycle
+        tempListOfValidEdges = [] +listOfEdges
+        tempListOfValidEdges.append(newEdge);
+        pastVertices = []
+        liveVertices = []
+        liveVertices.append(tempNode);
+        nextVertices = []
+        isCycleFound = False;
+        isPathFlowing = False;
+        while (liveVertices):
+            nextVertices.clear();
+            for presentVertex in liveVertices:
+                if (presentVertex.nextVertex != []):
+                    pastVertices.append(presentVertex); 
+                    for next in  presentVertex.nextVertex:
+                        isPathFlowing = (next in tempListOfValidEdges);
+                        if ((not next.vertex2 in pastVertices) and (next.vertex2 != None) and (isPathFlowing)):                           
+                           nextVertices.append(next.vertex2);
+                        isCycleFound = (next.vertex2 in pastVertices)
+                        if(isCycleFound):
+                            nextVertices.clear();
+                            liveVertices.clear();
+             
+            liveVertices.clear();
+            liveVertices += nextVertices;
+        return isCycleFound;
+
+
+    def makingMinSpanningTree(self):
+        
+
+        tempNode = self.startNode;
+
+        #Transversal code
+        #Sort to Asc
+        sortedEdges = [] + self.allEdges;
+        minSpanTree = []
+        for k in range( len(sortedEdges)):
+            for j in range(len(sortedEdges)):
+                if (sortedEdges[k].edge < sortedEdges[j].edge):
+                    sortedEdges[k],sortedEdges[j] = sortedEdges[j], sortedEdges[k]
+        for ordEdge in sortedEdges:
+            if not self.isCycle(minSpanTree, ordEdge):
+                minSpanTree.append(ordEdge);
+        return minSpanTree;
 
     def printGraph(self):
         ''''''
@@ -53,7 +109,31 @@ class Graph:
                     print(printGraph);
             liveVertices.clear();
             liveVertices += nextVertices;
+    '''
+    def kruskal(self):
+        # Sort all the edges in non-decreasing order of their weight
+        sorted_edges = sorted(self.allEdges, key=lambda x: x.edge)
 
+        # Initialize disjoint sets for each vertex
+        disjoint_sets = {vertex: {vertex} for vertex in [edge.vertex for edge in self.allEdges]}
+
+        # Initialize the minimum spanning tree
+        minimum_spanning_tree = []
+
+        for edge in sorted_edges:
+            set1 = next((s for s in disjoint_sets.values() if edge.vertex in s), set())
+            set2 = next((s for s in disjoint_sets.values() if edge.vertex2 in s), set())
+
+            if set1 != set2:
+                # If adding this edge doesn't form a cycle, add it to the minimum spanning tree
+                minimum_spanning_tree.append(edge)
+                # Merge the disjoint sets of the two vertices connected by this edge
+                new_set = set1.union(set2)
+                for vertex in new_set:
+                    disjoint_sets[vertex] = new_set
+
+        return minimum_spanning_tree
+        '''
                 
 
 if __name__ == "__main__":
@@ -67,16 +147,18 @@ if __name__ == "__main__":
     graph = Graph();
     graph.addNewEdge(a,b,3);
     graph.addNewEdge(b,c,2);
-    graph.addNewEdge(a,c,4);
+    graph.addNewEdge(c,d,4);
+    graph.addNewEdge(c,a,4);
+
 
 
     print("Finished loading Graph...");
     print()
-
-    print("Edges of Graph:")
-    print()
-   
+    print("Edges of Graph:")    
     graph.printGraph();
+
+    print()
+    print("===========================")
 
 
 
